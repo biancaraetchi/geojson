c2ba51b8712c758e4a2b1a41ba30729dbf937512
biancaraetchi <mold.academ@gmail.com>
2024-07-16 18:36:41 +0300

    Added CORS headers

--- be/geojson/settings.py
+++ be/geojson/settings.py
@@ -44,7 +44,8 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "rest_framework",
-    "rest_framework_swagger"
+    "rest_framework_swagger",
+    "corsheaders"
 ]
 
 MIDDLEWARE = [
@@ -55,7 +56,10 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
 ]
+
+CORS_ORIGIN_ALLOW_ALL = True
 
 ROOT_URLCONF = "geojson.urls"
 
