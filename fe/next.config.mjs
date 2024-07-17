fb0158d09cbe1e476f6170547dc3fcf2c221d586
biancaraetchi <mold.academ@gmail.com>
2024-07-17 21:10:25 +0300

    fix: added back next.config

---
+++ fe/next.config.mjs
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    reactStrictMode: false,
+    env:{
+        username:"admin",
+        password:"admin"
+    }
+};
+
+export default nextConfig;
